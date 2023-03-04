65d79bb17383300c5860f1b22b37954db3d7f61e
MAHIRAJ SINGH BAGHELA <126379542+mahiraj7@users.noreply.github.com>
2023-03-05 01:23:36 +0530

    day-001-exercise

--- day-001/exercise-level-1.py
+++ day-001/exercise-level-1.py
@@ -1,2 +1,28 @@
+#Question 1
 #Open the python interactive shell and do the following operations. The operands are 3 and 4.
 print (3+4)  #addititon 
+print (3-4)  #subtraction
+print (3*4)  #multiplication
+print (3%4)  #modulus
+print (3**4) #exponential
+print (3/4)  #division
+print (3//4) #floor division operator
+
+
+#Write strings on the python interactive shell. The strings are the following:
+#first_name
+#last_name
+#country
+#age
+#is_married
+#skills
+
+{
+'first_name':'Mahiraj',
+'last_name':'Baghela',
+'country':'India', 
+'age':18, 
+'is_married':False,
+'skills':['JS', 'React', 'Node', 'Python']
+}
+
---
+++ day-001/exercise-level-2.py
@@ -0,0 +1,10 @@
+#Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex number
+print(type('Mahiraj'))           # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Mahiraj'}))  # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
