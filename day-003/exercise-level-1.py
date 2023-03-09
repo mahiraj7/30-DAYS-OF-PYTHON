69d458fe6b139062e2bb8f59c3da1bf27c6ffd91
MAHIRAJ SINGH BAGHELA <126379542+mahiraj7@users.noreply.github.com>
2023-03-10 00:01:34 +0530

    day-003

---
+++ day-003/exercise-level-1.py
@@ -0,0 +1,29 @@
+#questions
+# 1.Declare your age as integer variable
+# 2.Declare your height as a float variable
+# 3.Declare a variable that store a complex number
+# 4.Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+
+print ("age: 18")
+print ("height: 6.2ft")
+print ("the area of the triangle is 100")
+
+
+# 5.Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
+#Enter side a: 5
+#Enter side b: 4
+#Enter side c: 3
+print ("perimeter = 5+4+3")
+
+#6. Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+l = float (input('5'))
+b = float (input ('4'))
+Area = l*b 
+
+perimeter = 2*(5+4)
+print ("area of a rectangle is : %.2f" %Area )
+
+
+
+
+
