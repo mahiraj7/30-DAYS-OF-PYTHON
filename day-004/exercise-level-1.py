d795d97fd048b7058071882fd179c7377b56c8a8
MAHIRAJ SINGH BAGHELA <126379542+mahiraj7@users.noreply.github.com>
2023-03-13 00:09:29 +0530

    day-004

--- day-004/exercise-level-1.py
+++ day-004/exercise-level-1.py
@@ -5,7 +5,6 @@ result = ' '.join(string)
 print (result)
 
 
-
 # 2.Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
 
 string = [ 'Coding', 'For' , 'All']
@@ -18,6 +17,33 @@ print (result)
 challenge = 'thirty_days_coding_for_all'
 print(challenge.isidentifier())
 
+
 # 4.Print the length of the company string using len() method and print().
+
 string = 'thirty_days_coding_for_all'
 print(len(string))
+
+
+# 5.Change all the characters to uppercase letters using upper() method.
+
+string= 'THIRTY_DAYS_CODING_FOR_ALL'
+print(challenge.isupper())
+
+
+#6. Change all the characters to lowercase letters using lower() method.
+
+string= 'thirty_days_coding_for_all'
+print(challenge.islower())
+
+
+#7.Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+string = 'thirty_days_coding_for_all'
+print(string.capitalize())
+print(string.title())
+print(string.swapcase())
+
+
+
+
+
+
