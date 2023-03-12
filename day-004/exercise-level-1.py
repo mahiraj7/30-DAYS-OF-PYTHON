63acd077f4425958ec4b4f6719150ed857ada161
MAHIRAJ SINGH BAGHELA <126379542+mahiraj7@users.noreply.github.com>
2023-03-13 00:07:22 +0530

    day-004

---
+++ day-004/exercise-level-1.py
@@ -0,0 +1,23 @@
+# 1.Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+
+string= ['Thirty', 'Days', 'Of', 'Python']
+result = ' '.join(string)
+print (result)
+
+
+
+# 2.Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+
+string = [ 'Coding', 'For' , 'All']
+result = ' '.join(string)
+print (result)
+
+
+# 3.Declare a variable named company and assign it to an initial value "Coding For All".
+
+challenge = 'thirty_days_coding_for_all'
+print(challenge.isidentifier())
+
+# 4.Print the length of the company string using len() method and print().
+string = 'thirty_days_coding_for_all'
+print(len(string))
